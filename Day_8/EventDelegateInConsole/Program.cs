6892360d6edd41a9677801717fe217b06e253bb5
itsdev04 <imkrdev@gmail.com>
2026-01-14 11:54:32 +0530

    commits

---
+++ Day_8/EventDelegateInConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EventDelegateInConsole;
+
+class Program
+{
+    public Program()
+    {
+        myevent = new mydelegate1(testFunction);
+    }
+    public void testFunction()
+    {
+        Console.WriteLine("Test function called");
+    }
+    public delegate void mydelegate1();
+    public event mydelegate1 myevent;
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        p.myevent();
+        //above two lines can be written in a single line like this : 
+        new Program().myevent();
+        Console.ReadLine();
+    }
+}
---
+++ Day_8/LambdaUsingDelegate/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LambdaUsingDelegate;
+
+class Program
+{
+    // public static void add(int x, int y)
+    // {
+    //     Console.WriteLine($"The sum is : {x+y}");
+    // }
+    // public static int substract(int x, int y)
+    // {
+    //     return x - y;
+    // }
+    // public static int multiply(int x, int y)
+    // {
+    //     return x * y;
+    // }
+    // public static void divide(int x, int y)
+    // {
+    //     Console.WriteLine($"The division is : {x/y}");
+    // }
+
+    public delegate void mydelegate1(int a, int b);
+    public delegate int mydelegate2(int a, int b);
+    static void Main(string[] args)
+    {
+        mydelegate1 m1 = (x,y) => Console.WriteLine($"The sum is : {x+y}");
+        m1 += (x,y) => Console.WriteLine($"The division is : {x/y}");
+        mydelegate2 m2 = (x,y) => x - y;
+        m2 += (x,y) => x * y;
+        foreach(mydelegate2 del in m2.GetInvocationList())
+        {
+            Console.WriteLine($"Result : {del(50,10)}");
+        }
+        Console.ReadLine();
+    }
+}
